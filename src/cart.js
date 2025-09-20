ef6806a7240eb7c9ea1faa2ecdefa02677c048a4
Licenia Alvear <licialvear@gmail.com>
2025-09-19 22:30:37 -0500

    feat: agregar integracion con checkout de stripe

---
+++ src/cart.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("payment-form");
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const res = await fetch("http://localhost:3000/create-checkout-session", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        nombre: form.nombre.value,
+        email: form.email.value
+      }),
+    });
+
+    const data = await res.json();
+    window.location.href = data.url;
+  });
