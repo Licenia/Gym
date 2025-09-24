eceb20d56183a721c71def3f6d50cb1788f957d3
Licenia Alvear <licialvear@gmail.com>
2025-09-23 22:23:29 -0500

    feat(nav-mobile): agregar  menu de hamburgesa con estilos y funcionamiento completo

---
+++ src/btn-hamburger.js
@@ -0,0 +1,15 @@
+const d = document,
+  btn = d.querySelector(".btn-hamburger"),
+  panel = d.querySelector(".panel");
+
+d.addEventListener("click", (e) => {
+  if (e.target.closest(".btn-hamburger")) {
+    btn.classList.toggle("is-active");
+    panel.classList.toggle("is-active");
+  }
+
+  if (e.target.matches(".menu a")) {
+    btn.classList.remove("is-active");
+    panel.classList.remove("is-active");
+  }
+});
